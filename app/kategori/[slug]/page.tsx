6c6da8e245e7c9cd90f0c2b7d489e71b39f2fdd8
omerfarukkaplan <admin@postmeetingfollowup.com>
2026-02-18 11:27:35 +0300

    Update page.tsx

--- app/kategori/[slug]/page.tsx
+++ app/kategori/[slug]/page.tsx
@@ -2,12 +2,10 @@ export default function CategoriesPage() {
   return (
     <div className="container">
       <h1>Kategoriler</h1>
-
       <div className="grid">
-        <div className="card">Fitness</div>
-        <div className="card">Diyetisyen</div>
-        <div className="card">Freelancer</div>
-        <div className="card">Danışmanlık</div>
+        <div className="card glass">Fitness</div>
+        <div className="card glass">Diyet</div>
+        <div className="card glass">Freelancer</div>
       </div>
     </div>
   );
