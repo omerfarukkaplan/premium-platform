8bab81c510851ea3cf31669e2895cd25cc79f036
omerfarukkaplan <admin@postmeetingfollowup.com>
2026-02-18 11:33:34 +0300

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,20 +1,19 @@
-import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const q = searchParams.get("q");
 
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
   const { data } = await supabase
     .from("seller_profiles")
     .select("*")
-    .ilike("title", `%${q}%`)
-    .eq("is_active", true);
+    .ilike("title", `%${q}%`);
 
   return NextResponse.json(data);
 }
