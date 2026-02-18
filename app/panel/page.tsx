be05a08d44eca15bd72529edb8508ad8b25080cb
omerfarukkaplan <admin@postmeetingfollowup.com>
2026-02-18 11:28:22 +0300

    Update page.tsx

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -2,14 +2,7 @@ export default function UserPanel() {
   return (
     <div className="container">
       <h1>Kullanıcı Paneli</h1>
-
-      <div className="card">
-        <p>Favori uzmanların</p>
-      </div>
-
-      <div className="card">
-        <p>Geçmiş işlemler</p>
-      </div>
+      <div className="card glass">Geçmiş İşlemler</div>
     </div>
   );
 }
